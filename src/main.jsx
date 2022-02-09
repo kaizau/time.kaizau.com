a1f661e452ebfc6bc628812789ecbf68fc7dd4db
Kai Zau <kaizau@users.noreply.github.com>
2022-02-09 17:12:06 +0800

    Try lowercase filename

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { render } from "preact";
 import { html } from "htm/preact";
-import { App } from "./App.jsx";
+import { App } from "./app.jsx";
 import "./index.css";
 
 render(html`<${App} />`, document.getElementById("App"));
