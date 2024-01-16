83516db866a9ebc5b46eb2ba33c4ac75418875cf
Kai Zau <kaizau@users.noreply.github.com>
2024-01-16 21:32:46 +0800

    Update vite build

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,5 +3,13 @@ import preact from "@preact/preset-vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        index: "index.html",
+        genie: "genie.html",
+      },
+    },
+  },
   plugins: [preact()],
 });
