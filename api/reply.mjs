32310b12538c198441df79c9df806aed49ecc693
Kai Zau <kaizau@users.noreply.github.com>
2024-01-17 14:15:18 +0800

    Working form parsing

--- api/reply.mjs
+++ api/reply.mjs
@@ -1,35 +1,54 @@
+import { Readable } from "stream";
 import busboy from "busboy";
 
 export default async (req /* , ctx */) => {
-  const fields = {};
-  const files = [];
-
-  const bb = busboy({ headers: req.headers });
-
-  bb.on("field", (key, val) => {
-    fields[key] = val;
-  });
-
-  bb.on("file", (name, file, info, encoding, mime) => {
-    let content = Buffer.from("");
-    file
-      .on("data", (data) => {
-        content = Buffer.concat([content, data]);
-      })
-      .on("close", () => {
-        files.push({ name, file, info, encoding, mime, content });
-      });
-  });
-
-  const finished = new Promise((resolve, reject) => {
-    busboy.on("finish", () => resolve());
-    busboy.on("error", (error) => reject(error));
-  });
-
-  await finished;
+  const [fields, files] = await parseForm(req);
 
   console.log("Fields:", fields);
   console.log("Files:", files);
 
   return new Response("🧞‍♂️");
 };
+
+async function parseForm(req) {
+  const fields = {};
+  const files = [];
+
+  const headers = Object.fromEntries(req.headers.entries());
+  const bb = busboy({ headers });
+
+  bb.on("field", (key, val) => {
+    fields[key] = val;
+  });
+
+  bb.on("file", (name, file, info) => {
+    let content = Buffer.from("");
+    file
+      .on("data", (data) => {
+        content = Buffer.concat([content, data]);
+      })
+      .on("end", () => {
+        files.push({ ...info, content });
+      });
+  });
+
+  const finished = new Promise((resolve, reject) => {
+    bb.on("finish", () => resolve([fields, files]));
+    bb.on("error", (error) => reject(error));
+  });
+
+  const stream = await convertToNodeStream(req.body);
+  stream.pipe(bb);
+  return await finished;
+}
+
+async function convertToNodeStream(webStream) {
+  const nodeStream = new Readable({
+    read() {},
+  });
+  for await (const chunk of webStream) {
+    nodeStream.push(Buffer.from(chunk));
+  }
+  nodeStream.push(null);
+  return nodeStream;
+}
