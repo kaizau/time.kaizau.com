874389e64881c6658677c17974c55af68e0c3bb9
Kai Zau <kaizau@users.noreply.github.com>
2024-01-17 10:47:37 +0800

    Parse attachments as file

--- api/reply.mjs
+++ api/reply.mjs
@@ -1,16 +1,28 @@
 export default async (req /* , ctx */) => {
   let data = {};
+  let attachments = [];
   try {
     const formData = await req.formData();
     for (const [key, value] of formData) {
-      data[key] = value;
+      if (value instanceof File) {
+        const attachment = {
+          filename: value.name,
+          type: value.type,
+          size: value.size,
+          content: await value.arrayBuffer(),
+        };
+        attachments.push(attachment);
+      } else {
+        data[key] = value;
+      }
     }
-    data.envelope = JSON.parse(data.envelope);
   } catch (e) {
-    console.log("Ignoring invalid request");
+    console.log("Ignoring invalid request", e);
     return new Response("🏖️");
   }
 
   console.log(data);
+  console.log(attachments);
+
   return new Response("ok");
 };
