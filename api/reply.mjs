d8c65b2e6358f86f363e81f57574d2b2a27a4a28
Kai Zau <kaizau@users.noreply.github.com>
2024-01-17 12:44:39 +0800

    Parse req.body instead

--- api/reply.mjs
+++ api/reply.mjs
@@ -1,16 +1,8 @@
-import { Readable } from "stream";
 import { IncomingForm } from "formidable";
 
 export default async (req /* , ctx */) => {
-  // Convert to stream for formidable, since Netlify functions don't
-  // seem to provide a standard IncomingMessage
-  const stream = new Readable();
-  stream.push(req.body);
-  stream.push(null); // end of stream
-  stream.headers = req.headers;
-
   const form = new IncomingForm();
-  form.parse(stream, (err, fields, files) => {
+  form.parse(req.body, (err, fields, files) => {
     if (err) {
       console.error("Ignoring invalid request:", err.message);
       return new Response("🏖️");
