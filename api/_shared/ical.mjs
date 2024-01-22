973fabade4cd402413cb06d17a57e7c9f6d866bb
Kai Zau <kaizau@users.noreply.github.com>
2024-01-22 16:56:50 +0800

    Fix rsvp typo

--- api/_shared/ical.mjs
+++ api/_shared/ical.mjs
@@ -109,8 +109,6 @@ export function updateRsvp(data, email, status) {
   const rsvpStr = rsvp.join(",");
   const [description, urlStr] = data.description.split(descriptionText);
   const url = new URL(urlStr.trim());
-  url.searchParams.delete("rspv");
-  url.searchParams.set("rspv", rsvpStr);
-  console.log(rsvpStr);
+  url.searchParams.set("rsvp", rsvpStr);
   data.description = `${description}${descriptionText}${url.toString()}`;
 }
